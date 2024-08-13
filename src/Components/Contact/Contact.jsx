a89f6b53ce5be5f8553e508a68ce7ef3f2b05369
Urimella Uma sri <48849066+umasriurimella@users.noreply.github.com>
2024-08-13 07:32:48 +0530

    Update Contact.jsx

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -41,7 +41,7 @@ const Contact = () => {
         <h3>Send us a message <img src={msg_icon} alt="" /></h3>
         <p>Feel free to reach out through contact form or find our contact information below. Your feedback, questions, and suggestions are important to us as we strive to provide exceptional service to our university community.</p>
         <ul>
-            <li><img src={mail_icon} alt="" />Contact@GreatStack.dev</li>
+            <li><img src={mail_icon} alt="" />Contact@Edusity.dev</li>
             <li><img src={phone_icon} alt="" />+1 123-456-7890</li>
             <li><img src={location_icon} alt="" />77 Massachusetts Ave, Cambridge<br/> MA 02139, United States</li>
         </ul>
